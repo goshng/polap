59595a63eb671405915548942911e45162ea6fc4
Sang Chul Choi <goshng@yahoo.co.kr>
2024-11-09 15:51:27 +0900

    Update the Bioconda recipe of Polap

--- src/polap-git-hash-version.sh
+++ src/polap-git-hash-version.sh
@@ -1,2 +1,2 @@
-# Commit hash: 65e5da846aa05e08d0a09f7d3c4e6919ffe9a49f
-_polap_git_hash_version='65e5da8'
+# Commit hash: 4bccf9694145045ec586c1d103c9c2e0a9d0e029
+_polap_git_hash_version='4bccf96'

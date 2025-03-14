b47fd10bc23bec89664379e359f4252f95c4ad29
Sang Chul Choi <goshng@yahoo.co.kr>
2025-03-14 12:50:54 +0900

    Update _POLAP_RELEASE=1 in polap.
    _POLAP_RELEASE=0 in polap.sh
    _POLAP_RELEASE=1 in polap.

--- src/polap-git-hash-version.sh
+++ src/polap-git-hash-version.sh
@@ -1,2 +1,2 @@
-# Commit hash: 17ce50554fb8d5c15d8cc8e4e7433c3869bcd032
-_polap_git_hash_version='17ce505'
+# Commit hash: db9781df9f9ba6f73a800420a36a48bcd583dbed
+_polap_git_hash_version='db9781d'
